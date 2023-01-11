9303f45d963ae2cf1610a3b5221a04089d7d2356
urieisenberg <urieisenberg@gmail.com>
2023-01-11 16:07:19 +0200

    refactoring cart like products to match contexts

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,17 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+  el.innerHTML = cartText;
+};
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+// Context/Situation #1
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+  if (el) {
+    mount(el);
+  }
+}
+
+// Context/Situation #2 for container app
+export { mount };
