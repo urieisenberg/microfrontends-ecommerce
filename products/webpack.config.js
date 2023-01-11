d475d37702647e316439802093d6d3d4bf17adeb
urieisenberg <urieisenberg@gmail.com>
2023-01-11 16:04:14 +0200

    so mount func can be imported

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: 'products',
       filename: 'remoteEntry.js', //for lists different modules that can be loaded from the Products project
       exposes: {
-        './ProductsIndex': './src/index',
+        './ProductsIndex': './src/bootstrap',
       },
       shared: ['faker'],
     }),
