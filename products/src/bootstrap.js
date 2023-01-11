4e6375997e5a6c97dbed5dc1b30a91c36d7f8e71
urieisenberg <urieisenberg@gmail.com>
2023-01-11 15:59:21 +0200

    refactor to match both contexts

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,23 +1,28 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 10; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
-}
+  for (let i = 0; i < 10; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
 
-document.querySelector('#dev-products').innerHTML = products;
+  el.innerHTML = products;
+};
 
 //sub app execution context
 //Context #1:
 // file running in development in isolation
 // using local index.html file
-// which DEFINITELY has an element with an id of 'dev-products' 
-// need to immediately render our app into that element 
+// which DEFINITELY has an element with an id of 'dev-products'
+// need to immediately render our app into that element
+
 
 //Context #2:
 // file is running in development or production
 // through the CONTAINER app
-// no gurantee that an element with an id of 'dev-products' exists  
-// we do not want to try to immediately render the app  
+// no gurantee that an element with an id of 'dev-products' exists
+// we do not want to try to immediately render the app
+
+
